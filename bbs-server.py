3d364e4dc3a4dc5bc32f83bd6ab070ed5205f614
creeper6530 <o.behal@centrum.cz>
2023-06-29 15:14:48 +0200

    Added checking of token DB; example post

--- bbs-server.py
+++ bbs-server.py
@@ -63,7 +63,10 @@ def load_db():
                 "posts": []},
 
                 {"name": "Technika",
-                "posts": []}]
+                "posts": [
+                    {"title": "LOLZ",
+                    "contents": "No contents",
+                    "id": 2}]}]
 
     try:
         with open(f"bbs_data{dividor}users.json", "r") as file:
@@ -98,6 +101,15 @@ def load_db():
                 {"user": "guest3",
                 "token": "CKJbn897hds=",
                 "valid_until": 1470987405}]
+        
+    print("Checking token DB...")
+    tmp_pair_list = token_pair_list.copy() # Funkce copy() zabraňuje vzniku reference na objekt token_pair_list
+                                           # Tvorba kopie je potřeba, aby for smyčka nic nepřeskočila
+    for token_pair in tmp_pair_list:
+        if token_pair["valid_until"] < int(time()):
+            token_pair_list.remove(token_pair)
+    del tmp_pair_list # Smažeme kopii, abychom ušetřili paměť
+
 load_db()
 
 def save_db():
